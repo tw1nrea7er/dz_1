b96c13bb618e3b1d5222995190d3514c7f10803e
Nickolay <katin@mephi.rugit config --global user.email katin@mephi.ru>
2023-04-24 23:29:10 +0300

    dz_1

---
+++ task1/Program.cs
@@ -0,0 +1,20 @@
+﻿//  Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+        // a = 5; b = 7 -> max = 7
+        // a = 2 b = 10 -> max = 10
+        // a = -9 b = -3 -> max = -3
+        Console.Write("Введите число: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите число: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+        int max = 0;
+        if (a > b)
+        {
+            max = a;
+            Console.WriteLine($"максимальное число из двух: {max}");
+        }
+        else
+        {
+            max = b;
+            Console.WriteLine($"максимальное число из двух: {max}");
+        }
---
+++ task2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+    Console.Write("Введите первое число: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите второе число: ");
+    int b = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите третье число: ");
+    int c = Convert.ToInt32(Console.ReadLine());
+    int max = 0;
+    if ((a >= b) && (a >= c))
+    {
+        max = a;
+        Console.WriteLine($"максимальное число из двух: {max}");
+    }
+    else if ((b >= a) && (b >= c))
+    {
+        max = b;
+        Console.WriteLine($"максимальное число из двух: {max}");
+    }
+    else if ((c >= a) && (c >= b)) 
+    {
+        max = c;
+        Console.WriteLine($"максимальное число из двух: {max}");
+    }
